5b1123bad34f7d9daa3cc672f002b7ea0293669e
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-06-02 22:23:11 -0300

    Js and Bootstrap

---
+++ 2ºBimestre/aula04/js.js
@@ -0,0 +1,20 @@
+
+// alert('Salve homanos!!!!')   
+
+function btn() {
+    alert('Salve homanos!!!!') 
+}
+
+var url = 'https://www.google.com/'
+
+function salve() {
+    window.open(url, '_blank')
+}
+
+
+var nome = 'Bruna'
+var idade = 19
+document.write('<p>Olá ' + nome + '</p>')
+document.write('<p>Idade: ' + idade  + '</p>')
+
+document.write('<p>Olá ' + nome + ', tudo bem ? É verdade que você tem ' + idade + ' anos </p>')
